af2cee4393b87c859a31be8c7a13e2186d4f9590
Oiva Eskola <oiva.eskola@gmail.com>
2013-11-17 23:09:50 +0200

    add support for reading handles from chrome.storage

--- js/content.js
+++ js/content.js
@@ -1,7 +1,24 @@
 var NoRetweet = (function() {
   'use strict';
 
-  var onDomNodeInserted, queue, run, setupListeners, throttleTimeout = null;
+  var getHandles, init, onDomNodeInserted, onStorageChanged, queue, run,
+    setupListeners;
+  
+  var handles = [], throttleTimeout = null;
+
+  getHandles = function() {
+    chrome.storage.local.get('handles', function (result) {
+      if (result.handles !== undefined) {
+        handles = result.handles;
+        console.log('got handles', handles);
+      }
+    });
+  };
+
+  init = function() {
+    getHandles();
+    setupListeners();
+  };
 
   /* see if a new retweet was added to DOM, queue removal */
   onDomNodeInserted = function(event) {
@@ -15,6 +32,10 @@ var NoRetweet = (function() {
     }
   };
 
+  onStorageChanged = function(changes, namespace) {
+    console.log(changes, namespace);
+  };
+
   queue = function() {
     if (throttleTimeout === null) {
       throttleTimeout = setTimeout(run, 10);
@@ -23,11 +44,9 @@ var NoRetweet = (function() {
 
   run = function() {
     console.log('run no-retweet');
-    var names = ['marcoarment', 'mikko'];
-      
     _.each(document.querySelectorAll('[data-retweeter]'), function(element) {
       var retweeter = element.getAttribute('data-retweeter');
-      if (_.contains(names, retweeter)) {
+      if (_.contains(handles, retweeter)) {
         console.log('remove retweet by '+retweeter);
         element.remove();
       }
@@ -42,14 +61,15 @@ var NoRetweet = (function() {
     // tweet list
     var list = document.querySelector('#stream-items-id');
     list.addEventListener('DOMNodeInserted', onDomNodeInserted);
+
+    // storage listener
+    chrome.storage.onChanged.addListener(onStorageChanged);
   };
 
   return {
     run: run,
-    setupListeners: setupListeners
+    init: init
   };
 })();
 
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {});
-
-NoRetweet.setupListeners();
+NoRetweet.init();
