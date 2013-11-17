abe744d28945f5339670dc421db340d1f73b90ab
Oiva Eskola <oiva.eskola@gmail.com>
2013-11-17 21:17:51 +0200

    Run retweet removal when new tweets are inserted
    We now listen to DOM node inserts, and determine if a retweet was added
    to the DOM. We then queue a retweet removal run.

--- js/content.js
+++ js/content.js
@@ -1,6 +1,29 @@
 var NoRetweet = (function() {
-  var run = function() {
-    var names = ['marco', 'mikko'];
+  'use strict';
+
+  var onDomNodeInserted, queue, run, setupListeners, throttleTimeout = null;
+
+  /* see if a new retweet was added to DOM, queue removal */
+  onDomNodeInserted = function(event) {
+    var children, el = event.target;
+    if (el.tagName === 'LI') {
+      children = el.getElementsByTagName('div');
+      if (children.length && children[0].hasAttribute('data-retweeter')) {
+        console.log('retweet inserted');
+        queue();
+      }
+    }
+  };
+
+  queue = function() {
+    if (throttleTimeout === null) {
+      throttleTimeout = setTimeout(run, 10);
+    }
+  };
+
+  run = function() {
+    console.log('run no-retweet');
+    var names = ['marcoarment', 'mikko'];
       
     _.each(document.querySelectorAll('[data-retweeter]'), function(element) {
       var retweeter = element.getAttribute('data-retweeter');
@@ -9,15 +32,24 @@ var NoRetweet = (function() {
         element.remove();
       }
     });
+    throttleTimeout = null;
+  };
+
+  setupListeners = function() {
+    console.log('setup event listeners');
+    window.onpopstate = run;
+
+    // tweet list
+    var list = document.querySelector('#stream-items-id');
+    list.addEventListener('DOMNodeInserted', onDomNodeInserted);
   };
 
   return {
-    run: run
+    run: run,
+    setupListeners: setupListeners
   };
 })();
 
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {});
 
-window.onpopstate = function (event) {
-  NoRetweet.run();
-}
+NoRetweet.setupListeners();
