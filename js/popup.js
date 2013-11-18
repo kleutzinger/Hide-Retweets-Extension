c3f795daa19cd03f6ffdee41f7851fc77a10557a
Oiva Eskola <oiva.eskola@gmail.com>
2013-11-18 22:10:25 +0200

    redraw list of handles when syncing list / adding to it
    Use storage.sync everywhere. Use renderHandles to redraw the list of
    handles.

--- js/popup.js
+++ js/popup.js
@@ -1,46 +1,59 @@
 var popup = (function() {
   'use strict';
 
-  var addHandle, getHandles, removeHandle, setupElements, setupListeners, 
-    syncHandles;
+  var addHandle, getHandles, removeHandle, renderHandles, setupElements,
+    setupListeners, syncHandles;
 
   var handles = [], inputEl, listEl, newRowEl;
 
   addHandle = function() {
-    var handle, row;
+    console.log('addHandle');
+    var handle;
 
     handle = inputEl.value;
     inputEl.value = '';
     
     // @oiva -> oiva
-    if (handle.substring(0, 1) == '@') {
+    if (handle.substring(0, 1) === '@') {
       handle = handle.substring(1);
     }
 
     console.log('addHandle '+handle);
     if (_.contains(handles, handle)) {
+      console.log('old');
       return;
     }
     handles.push(handle);
     syncHandles();
 
-    // insert element
-    row = '<li>@' + handle + '</li>';
-    newRowEl.insertAdjacentHTML('beforebegin', row);
+    renderHandles();
   };
 
   getHandles = function() {
     console.log('get handles');
-    chrome.storage.local.get('handles', function (result) {
+    chrome.storage.sync.get('handles', function (result) {
       if (result.handles !== undefined) {
+        console.log('got handles', result.handles);
+        console.log(this);
+        console.log(renderHandles);
         handles = result.handles;
-        console.log('got handles', handles);
+        renderHandles();
       }
     });
   };
 
   removeHandle = function() {
+    renderHandles();
+  };
 
+  renderHandles = function() {
+    var html = '', prevHandles = listEl.querySelectorAll('.handle');
+    _.invoke(prevHandles, 'remove');
+    
+    _.each(handles, function(handle) {
+      html += '<li class="handle">@' + handle + '</li>';
+    });
+    newRowEl.insertAdjacentHTML('beforebegin', html);
   };
 
   setupElements = function() {
@@ -54,8 +67,8 @@ var popup = (function() {
   };
 
   syncHandles = function() {
-    chrome.storage.local.set({'handles': handles}, function() {
-      message('Settings saved');
+    chrome.storage.sync.set({'handles': handles}, function() {
+      console.log('Settings saved');
     });
   };
 
